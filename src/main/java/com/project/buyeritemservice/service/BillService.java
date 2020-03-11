8df50c000f915ca0bdad4e28e61d3dc6c4982b5a
Prashanthi1098 <60173727+Prashanthi1098@users.noreply.github.com>
2020-03-11 18:15:57 +0530

    Add files via upload

---
+++ src/main/java/com/project/buyeritemservice/service/BillService.java
@@ -0,0 +1,10 @@
+package com.project.buyeritemservice.service;
+
+import java.util.Set;
+
+import com.project.buyeritemservice.pojo.BillPojo;
+
+public interface BillService {
+	BillPojo addBill(BillPojo billPojo);
+	Set<BillPojo> getBill(int buyerId);
+}
